4a3c87a35f31201b270ed7325465b5b8ccc960b8
Norbert <norbert.kulacin@gmail.com>
2020-06-26 16:20:36 +0200

    Updates

---
+++ precache-manifest.9545f339ec6b2070d6bfc112acb8ad6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41e07484894e87a3fb9dbf13fa0b497",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "24484b3e90fdacb90501",
+    "url": "/monster-rolodex/static/css/main.98bd403f.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24484b3e90fdacb90501",
+    "url": "/monster-rolodex/static/js/main.b161139b.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
